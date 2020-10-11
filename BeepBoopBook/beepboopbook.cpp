ecab33c195c6daf9ad7217924bb0b3aaf2d151da
MNJ <mnjones@gmail.com>
2020-10-11 18:53:20 +0100

    Initial attempt

--- BeepBoopBook/beepboopbook.cpp
+++ BeepBoopBook/beepboopbook.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
 #include "beepboopbook.h"
-#include "book.h"
 
 using namespace std;
 
-
-const int count_page_turns(Book& book, const int target_page, bool backwards=false)
+const int count_from_front(const int n, const int p)
 {
 	int count = 0;
-
-	while (!book.is_page_visible(target_page))
+	int right_page = 1;
+	int left_page = 0;
+	while (p > right_page)
 	{
 		count++;
-		book.turn_page(backwards);
+		right_page += 2;
+		left_page += 2;
+	}
+
+	return count;
+}
+
+const int count_from_back(const int n, const int p)
+{
+	int count = 0;
+	int left_page = int((n + 1) / 2) * 2;
+	int right_page =  left_page + 1;
+	while (left_page > p)
+	{
+		count++;
+		right_page -= 2;
+		left_page -= 2;
 	}
 
 	return count;
@@ -20,12 +35,20 @@ const int count_page_turns(Book& book, const int target_page, bool backwards=fal
 
 const int f(const int n, const int p)
 {
-	Book* book = new Book(n);
 	int count;
 
 	// Do we start from the front of the book, or from the back?
-	const int from_front = count_page_turns(*book, p);
-	const int from_back = count_page_turns(*book, p, true);
+	const int from_front = count_from_front(n, p);
+	const int from_back = count_from_back(n, p);
 
-	return from_front < from_back ? from_front : from_back;
+	if (from_back < from_front)
+	{
+		count = from_back;
+	}
+	else
+	{
+		count = from_front;
+	}
+
+	return count;
 }
